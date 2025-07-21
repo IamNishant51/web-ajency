a818c9126ad3665dcb9f40cb82ac28e1271e0a8c
IamNishant51 <anonymouslucifer400@gmail.com>
2025-07-21 06:16:11 +0000

    trying optmizing

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { visualizer } from 'rollup-plugin-visualizer'; // <-- Add this import
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
+  plugins: [tailwindcss(),react(),
+    visualizer({
+      open: true,       // Opens the report in your browser after build
+      gzipSize: true,   // Shows gzipped size
+      brotliSize: true, // Shows brotli size
+    }),
+  ],
 })
